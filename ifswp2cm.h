7ce6bc7b527e7413ba48542f77a88c2e9c880671
sincos2854 <sincos2854dev@gmail.com>
2026-05-23 10:15:25 +0900

    Use inline constexpr for header-defined string constants

--- ifswp2cm.h
+++ ifswp2cm.h
@@ -7,11 +7,11 @@
 #include "version.h"
 #include <string>
 
-constexpr LPCWSTR API_VER = L"00IN";
-constexpr LPCWSTR EXTENSION = L"*.wp2";
-constexpr LPCWSTR FILE_TYPE = L"WebP 2 file(*.wp2)";
+inline constexpr LPCWSTR API_VER = L"00IN";
+inline constexpr LPCWSTR EXTENSION = L"*.wp2";
+inline constexpr LPCWSTR FILE_TYPE = L"WebP 2 file(*.wp2)";
 
-constexpr std::wstring_view PLUGIN_INFO[]{
+inline constexpr std::wstring_view PLUGIN_INFO[]{
     API_VER,
     PLUGIN_NAME,
     EXTENSION,
